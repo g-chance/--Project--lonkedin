4c4812f97dbc8df9f56d946adcbd3d81afea1a4d
Greg Chance <ge.chance@gmail.com>
2020-04-28 10:54:55 -0700

    initial commit

---
+++ src/main/java/com/wabdavinc/lonkedin/controllers/MainController.java
@@ -0,0 +1,100 @@
+package com.wabdavinc.lonkedin.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.wabdavinc.lonkedin.models.User;
+import com.wabdavinc.lonkedin.repositories.GameRepo;
+import com.wabdavinc.lonkedin.repositories.JobRepo;
+import com.wabdavinc.lonkedin.repositories.PostRepo;
+import com.wabdavinc.lonkedin.repositories.SkillRepo;
+import com.wabdavinc.lonkedin.repositories.UserRepo;
+import com.wabdavinc.lonkedin.services.UserServ;
+import com.wabdavinc.lonkedin.validator.UserValidator;
+
+@Controller
+public class MainController {
+
+	private final UserRepo urepo;
+	private final UserServ userv;
+	private final UserValidator uvalid;
+	private final GameRepo grepo;
+	private final JobRepo jrepo;
+	private final PostRepo prepo;
+	private final SkillRepo srepo;
+	
+	public MainController(
+			UserRepo urepo,
+			UserServ userv,
+			UserValidator uvalid,
+			GameRepo grepo,
+			JobRepo jrepo,
+			PostRepo prepo,
+			SkillRepo srepo
+			) {
+		this.urepo = urepo;
+		this.userv = userv;
+		this.uvalid = uvalid;
+		this.grepo = grepo;
+		this.jrepo = jrepo;
+		this.prepo = prepo;
+		this.srepo = srepo;
+	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard(HttpSession session, Model model) {
+		if(session.getAttribute("user_id") == null) {
+			return "redirect:/login";
+		}
+		Long id = (Long) session.getAttribute("user_id");
+		model.addAttribute("user",urepo.findById(id).orElse(null));
+		return "dashboard.jsp";
+	}
+	
+	
+//	Login and Registration
+	
+	@GetMapping("/registration")
+	public String registerUser(Model model) {
+		model.addAttribute("user", new User());
+		return "register.jsp";
+	}
+	@PostMapping("/registration")
+	public String doRegisterUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+		uvalid.validate(user, result);
+		if(result.hasErrors()) {
+			return "register.jsp";
+		}
+		userv.registerUser(user);
+		session.setAttribute("user_id", user.getId());
+		return "redirect:/dashboard";
+	}
+	@GetMapping("/login")
+	public String login() {
+		return "login.jsp";
+	}
+	@PostMapping("/login")
+	public String doLogin(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
+		if(!userv.authenticateUser(email, password)) {
+			model.addAttribute("error", "Incorrect email / password combination");
+			return "login.jsp";	
+		}
+		User user = urepo.findByEmail(email);
+		session.setAttribute("user_id", user.getId());
+		return "redirect:/dashboard";
+	}
+	@GetMapping("logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/login";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/register.jsp
@@ -0,0 +1,43 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>   
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Registration Page</title>
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<script type="text/javascript" src="/js/app.js"></script>
+</head>
+<body>
+
+    <h1>Register!</h1>
+    
+    <p><form:errors path="user.*"/></p>
+    
+    <form:form method="POST" action="/registration" modelAttribute="user">
+        <p>
+            <form:label path="name">Name:</form:label>
+            <form:input type="text" path="name"/>
+        </p>
+        <p>
+            <form:label path="email">Email:</form:label>
+            <form:input type="email" path="email"/>
+        </p>
+        <p>
+            <form:label path="password">Password:</form:label>
+            <form:password path="password"/>
+        </p>
+        <p>
+            <form:label path="confirmPassword">Confirm Password:</form:label>
+            <form:password path="confirmPassword"/>
+        </p>
+        <input type="submit" value="Register!"/>
+    </form:form>
+    
+    <p>Already have an account? <a href="/login">Login</a></p>
+    
+</body>
+</html>
